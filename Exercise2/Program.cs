fe9707dfc40444a27c294936902968b7cea201db
Raw <tranduc@gmail.com>
2021-06-18 14:57:06 +0700

    done

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercise2
+﻿using System;
+
+namespace Exercise2
 {
     internal class Program
     {
@@ -10,6 +12,30 @@
         private static void SearchWord()
         {
             string str;
+            string wordS;
+            int count = 0;
+            Console.WriteLine("Enter a String");
+            str = Console.ReadLine();
+            Console.WriteLine("Enter a word to search");
+            wordS = Console.ReadLine();
+            string[] c = str.Split();
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (wordS != null && wordS.Equals(c[i]))
+                {
+                    count++;
+                }
+                
+            }
+
+            if (count <= 1 )
+            {
+                Console.WriteLine($"Word found {count} time in the string");
+            }
+            else
+            {
+                Console.WriteLine($"Word found {count} times in the string");
+            }
         }
     }
 }
